f38f19df5a889d6738c293b3dead149c7cc59965
Bimu.live <19252273+zhibocon@users.noreply.github.com>
2025-07-25 23:30:50 +0800

    Update config.js
    资料页底部也注释了

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,12 +51,12 @@ const CONFIG = {
    //  ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://www.youtube.com/@Bimuakung',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://bimu.tv',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+   //  HEO_INFO_CARD_URL1: '/about',
+   //  HEO_INFO_CARD_ICON1: 'fas fa-user',
+   //  HEO_INFO_CARD_URL2: 'https://www.youtube.com/@Bimuakung',
+   //  HEO_INFO_CARD_ICON2: 'fab fa-github',
+   //  HEO_INFO_CARD_URL3: 'https://bimu.tv',
+   //  HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
