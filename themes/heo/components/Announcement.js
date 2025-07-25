f520bba2b654dfc69aa9e156f42eb980967f874a
Bimu.live <19252273+zhibocon@users.noreply.github.com>
2025-07-26 00:12:34 +0800

    Update Announcement.js
    修改公告的显示

--- themes/heo/components/Announcement.js
+++ themes/heo/components/Announcement.js
@@ -5,10 +5,18 @@ const NotionPage = dynamic(() => import('@/components/NotionPage'))
 const Announcement = ({ post, className }) => {
   if (post?.blockMap) {
     return (
-      <div>
+      <div className={className}>
         {post && (
-          <div id='announcement-content'>
+          <div id="announcement-content">
             <NotionPage post={post} />
+            {/* 如果 post 中有头像数据，则显示头像 */}
+            {post.avatar && (
+              <img
+                src={post.avatar}
+                alt="Avatar"
+                className="announcement-avatar"
+              />
+            )}
           </div>
         )}
       </div>
@@ -17,4 +25,5 @@ const Announcement = ({ post, className }) => {
     return <></>
   }
 }
+
 export default Announcement
