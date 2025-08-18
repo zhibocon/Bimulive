209c178974ba879ea8bf3704780ab9ef7400a86a
Bimu.live <19252273+zhibocon@users.noreply.github.com>
2025-08-18 20:42:59 +0800

    Update config.js
    干货分享

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -31,7 +31,7 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '硬件体验', url: '/tag/硬件体验' },
-  HEO_HERO_CATEGORY_2: { title: '软件分享', url: '/tag/软件分享' },
+  HEO_HERO_CATEGORY_2: { title: '干货分享', url: '/tag/干货分享' },
   HEO_HERO_CATEGORY_3: { title: 'OBS教程', url: '/tag/OBS' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
